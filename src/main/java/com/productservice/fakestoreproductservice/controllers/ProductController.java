fd688b4cafe42d1312e8f5a3d6c3e039f877b292
ayush.agrawal698@gmail.com <ayush.agrawal698@gmail.com>
2024-05-25 16:49:56 +0530

    connecting product service to user service

--- src/main/java/com/productservice/fakestoreproductservice/controllers/ProductController.java
+++ src/main/java/com/productservice/fakestoreproductservice/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package com.productservice.fakestoreproductservice.controllers;
 
+import com.productservice.fakestoreproductservice.commons.AuthCommons;
+import com.productservice.fakestoreproductservice.dtos.UserDto;
 import com.productservice.fakestoreproductservice.exceptions.ProductNotFoundException;
 import com.productservice.fakestoreproductservice.models.Product;
 import com.productservice.fakestoreproductservice.services.ProductService;
@@ -19,14 +21,28 @@ import java.util.List;
 
 public class ProductController {
     private ProductService productService;
-    public ProductController(@Qualifier("SelfProductService") ProductService productService){
+    private AuthCommons authCommons;
+    public ProductController(@Qualifier("SelfProductService") ProductService productService,
+                             AuthCommons authCommons){
+        this.authCommons= authCommons;
         this.productService= productService;
     }
     //localhost:8080/products/1
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable("id") Long id) throws ProductNotFoundException {
-        Product product= productService.getProductById(id);
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Long id, @RequestHeader("authToken") String token) throws ProductNotFoundException {
+
+        //Call UserService validateToken API to validate the token.
+        UserDto userDto= authCommons.validateToken(token);
         ResponseEntity<Product> responseEntity;
+        if (userDto==null){
+            responseEntity = new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+            return responseEntity;
+        }
+
+        //Role based access
+
+
+        Product product= productService.getProductById(id);
         if(product==null) { // if product is not found, send status code 400:BAD_REQUEST
         //    throw new ProductNotFoundException(id+ "Product with id "+ id+ " not found.");
         }
